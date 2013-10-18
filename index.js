ca8666973fc9df04e57c3291d535384e12b7a16f
ahmad fanaei <fanaei.ahmad@gmail.com>
2013-10-18 22:43:43 +0330

    find the right place to work in

--- index.js
+++ index.js
@@ -4,4 +4,8 @@ var fs = require("fs");
 robot.login(function(err,result,body){
 	//fs.writeFile("web.txt", body );
 	console.log(result.statusCode);
+	var regex = /<table id="datagrid">(.|\n)*?<\/table>/;
+	var str ='<table id="datagrid">salam</table>';
+	var result = body.match(regex);
+	console.log(result[0]);
 });
